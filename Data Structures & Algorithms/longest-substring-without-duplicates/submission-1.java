072818ac97a3a03ee5bdb67529ff95a6fa3672fe
Harshad Yewale <153903433+Harshad-Yewale@users.noreply.github.com>
2026-05-27 10:40:21 +0530

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int maxLength=0;
+        int left=0;
+        HashSet<Character> set=new HashSet<>();
+
+        for(int right=0;right<s.length();right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength=Math.max(maxLength,set.size());
+
+        }
+
+        // for(int i=0; i<s.length();i++){
+        //      HashSet<Character> set=new HashSet<>();
+        //     for(int j=i; j<s.length();j++){
+                
+        //         if(set.contains(s.charAt(j))){
+        //             break;
+        //         }
+        //         else{
+        //             set.add(s.charAt(j));
+        //         }
+        //         maxLength=Math.max(maxLength,set.size());
+        //     }
+        // }
+        return maxLength;
+    }
+}
