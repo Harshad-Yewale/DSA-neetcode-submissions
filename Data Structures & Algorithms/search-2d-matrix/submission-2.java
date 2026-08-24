f5837ecd26b951a5983d34fb97e3a559c8e7c224
Harshad Yewale <153903433+Harshad-Yewale@users.noreply.github.com>
2026-08-24 17:53:06 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int subArrayIndex =-1;
+
+        for(int i=0; i<matrix.length; i++){
+            if(matrix[i][matrix[i].length-1] >= target){
+                subArrayIndex=i;
+                break;
+            }
+        }
+
+        if(subArrayIndex==-1){
+            return false;
+        }
+
+        int left = 0;
+        int right = matrix[subArrayIndex].length-1;
+
+
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            System.out.println(matrix[subArrayIndex][mid]);
+            if(matrix[subArrayIndex][mid] == target){
+                return true;
+            }
+            else if (matrix[subArrayIndex][mid] > target){
+                right = mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return false;
+    }
+}
