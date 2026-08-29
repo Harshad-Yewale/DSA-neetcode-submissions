8dce8daa2ed0483fd90d98b63a58eac526dc3696
Harshad Yewale <153903433+Harshad-Yewale@users.noreply.github.com>
2026-08-29 19:54:30 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,53 @@
+class TimeMap {
+
+    HashMap<String,List<Pair>> map;
+
+    static class Pair{
+        int stamp;
+        String value;
+
+        public Pair(int stamp, String value){
+            this.stamp = stamp;
+            this.value = value;
+        }
+    }
+
+    public TimeMap() {
+        map=new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+         if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Pair(timestamp, value));
+        
+    }
+    
+    public String get(String key, int timestamp) {
+         if (!map.containsKey(key)) {
+            return "";
+        }
+
+        List<Pair> list =  map.get(key);
+
+        int left=0;
+        int right=list.size()-1;
+        String ans="";
+
+        while(left<=right){
+            int mid = left +(right-left) /2;
+
+            if(list.get(mid).stamp<=timestamp){
+                ans=list.get(mid).value;
+                left=left+1;
+            }
+            else{
+                right=right-1;
+            }
+        }
+
+        return ans;
+        
+    }
+}
